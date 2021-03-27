825765ef80baa3f090afc8925266f1ae72fc08d0
PatriotRossii <rorkh@internet.ru>
2021-03-28 01:34:29 +0500

    Fix fmt

--- src/character_types/mod.rs
+++ src/character_types/mod.rs
@@ -30,15 +30,17 @@ pub enum ConsTrait {
 /// One of several classifications, enumerated in Figure 2-6, that are used for dispatch during parsing by the Lisp reader
 pub enum SyntaxType {
     /// Constituent characters are used in tokens
-    Constituent { r#traits: ConsTrait },
-    MacroCharacter { kind: MacroCharKind },
+    Constituent {
+        r#traits: ConsTrait,
+    },
+    MacroCharacter {
+        kind: MacroCharKind,
+    },
     SingleEscape,
     MultipleEscape,
     Whitespace,
     Invalid,
 }
-
-
 
 pub trait Character {
     fn syntax_type(&self) -> SyntaxType;
@@ -47,13 +49,44 @@ pub trait Character {
 impl Character for char {
     fn syntax_type(&self) -> SyntaxType {
         match self {
-            '\u{8}' => SyntaxType::Constituent { r#traits: ConsTrait::Invalid },
-            '!' | '$' | '%' | '&' | '*' | '+' | '-' | '<' | '=' | '>' | '?' | '@' | '[' | ']' | '^' | '_' | '{' | '}' | '~' => SyntaxType::Constituent { r#traits: ConsTrait::Alphabetic { ext: None }},
-            '.' => SyntaxType::Constituent { r#traits: ConsTrait::Alphabetic { ext: Some(AlphabeticExt::Dot { ext: Some(DotExt::DecimalPoint) }) } },
-            '/' => SyntaxType::Constituent { r#traits: ConsTrait::Alphabetic { ext: Some(AlphabeticExt::RatioMarker) } },
-            '0'..= '9' | 'a' ..= 'c' | 'A' ..= 'C' | 'g' ..= 'k' | 'G' ..= 'K' | 'm' ..= 'r' | 'M' ..= 'R' | 't' ..= 'z' | 'T' ..= 'Z' => SyntaxType::Constituent { r#traits: ConsTrait::Alphadigit { ext: None }},
-            'd' ..= 'f' | 'D' ..= 'F' | 'l' | 's' | 'L' | 'S' => SyntaxType::Constituent { r#traits: ConsTrait::Alphadigit { ext: Some(AlphadigitExt::ExponentMarker) }},
-            ':' => SyntaxType::Constituent { r#traits: ConsTrait::PackageMarker },
+            '\u{8}' => SyntaxType::Constituent {
+                r#traits: ConsTrait::Invalid,
+            },
+            '!' | '$' | '%' | '&' | '*' | '+' | '-' | '<' | '=' | '>' | '?' | '@' | '[' | ']'
+            | '^' | '_' | '{' | '}' | '~' => SyntaxType::Constituent {
+                r#traits: ConsTrait::Alphabetic { ext: None },
+            },
+            '.' => SyntaxType::Constituent {
+                r#traits: ConsTrait::Alphabetic {
+                    ext: Some(AlphabeticExt::Dot {
+                        ext: Some(DotExt::DecimalPoint),
+                    }),
+                },
+            },
+            '/' => SyntaxType::Constituent {
+                r#traits: ConsTrait::Alphabetic {
+                    ext: Some(AlphabeticExt::RatioMarker),
+                },
+            },
+            '0'..='9'
+            | 'a'..='c'
+            | 'A'..='C'
+            | 'g'..='k'
+            | 'G'..='K'
+            | 'm'..='r'
+            | 'M'..='R'
+            | 't'..='z'
+            | 'T'..='Z' => SyntaxType::Constituent {
+                r#traits: ConsTrait::Alphadigit { ext: None },
+            },
+            'd'..='f' | 'D'..='F' | 'l' | 's' | 'L' | 'S' => SyntaxType::Constituent {
+                r#traits: ConsTrait::Alphadigit {
+                    ext: Some(AlphadigitExt::ExponentMarker),
+                },
+            },
+            ':' => SyntaxType::Constituent {
+                r#traits: ConsTrait::PackageMarker,
+            },
             '\u{9}' | '\n' | '\u{C}' | '\u{D}' | '\u{20}' => SyntaxType::Whitespace,
             '"' | '\'' | '(' | ')' | ',' | ';' | '`' => SyntaxType::MacroCharacter {
                 kind: MacroCharKind::Terminating,
